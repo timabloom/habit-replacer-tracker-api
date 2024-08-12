a8c80da8c778d3f552d905a28809e62789ba3e45
timabloom <timabloom@proton.me>
2024-08-12 13:18:39 +0200

    feat: validations for post activity requests

--- HabitReplacerTracker/Models/AddActivityRequest.cs
+++ HabitReplacerTracker/Models/AddActivityRequest.cs
@@ -4,7 +4,9 @@ namespace HabitReplacerTracker.Models;
 
 public record AddActivityRequest(
     [Required]
+    [Length(10,10)]
     string Date,
     [Required]
+    [Range(1, 1440)]
     int Minutes
 );
