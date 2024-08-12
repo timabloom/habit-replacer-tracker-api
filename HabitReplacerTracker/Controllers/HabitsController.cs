fa210a0628fdc3e39f364b0431eb233589f0f15b
timabloom <timabloom@proton.me>
2024-08-12 11:37:35 +0200

    feat: created getall node

--- HabitReplacerTracker/Controllers/HabitsController.cs
+++ HabitReplacerTracker/Controllers/HabitsController.cs
@@ -4,7 +4,14 @@ namespace HabitReplacerTracker.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class Habits : ControllerBase
+public class HabitsController : ControllerBase
 {
+    private readonly IHabitsRepository _repo;
+    public HabitsController(IHabitsRepository repo)
+    {
+        _repo = repo;
+    }
 
+    [HttpGet]
+    public Habits GetHabits() => _repo.GetAllHabits();
 }
