c13a22e5d69966cfe57f3bc261bafb682ccec113
timabloom <timabloom@proton.me>
2024-08-14 09:58:14 +0200

    feat: allow 1 specic origin

--- HabitReplacerTracker/Program.cs
+++ HabitReplacerTracker/Program.cs
@@ -1,6 +1,17 @@
 using HabitReplacerTracker.Data;
 
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:5173");
+                      });
+});
 
 // Add services to the container.
 
@@ -19,6 +30,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
